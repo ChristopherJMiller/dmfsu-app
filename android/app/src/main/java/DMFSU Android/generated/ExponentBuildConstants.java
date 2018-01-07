50e4f9f5cbaf8a0478eb711557cee707088d1fa0
Christopher Miller <admin@mctherealm.net>
2018-01-07 03:28:36 +0300

    Relinked and ejected for IOS

--- android/app/src/main/java/DMFSU Android/generated/ExponentBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package DMFSU Android.generated;
-
-// This file is auto-generated. Please don't rename!
-public class ExponentBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "";
-
-}
